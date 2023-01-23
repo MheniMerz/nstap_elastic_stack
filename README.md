d8c12a21b632fa0089ddece24b59b30d0ad8973a
mhenimerz <hani.merzouki@gmail.com>
2023-01-23 15:06:34 +0000

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# nstap_elastic_stack
+# creating elastic stack cluster with terraform
+
+## perpare the host machines
+
+install virtualization packages
+
+```bash
+sudo apt install virt-manager virt-top libguestfs-tools  -y
+````
+
+disable apparmor or selinux (depending on the linux distribution) for qemu
+
+```bash
+echo "security_driver" >> /etc/libvirt/qemu.conf
+sudo systemctl restart libvirt-guests.service
+sudo systemctl restart libvirtd.service
+
+
+```
+## install terraform on your workstation or laptop
+
+```bash
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+sudo apt update && sudo apt install terraform
+
+```
+
+download and install the required terraform providers
+
+```bash
+mkdir -p $HOME/.local/share/terraform/plugins/registry.terraform.io/dmacvicar/libvirt/0.6.3/linux_amd64/
+
+cd !$
+
+wget https://github.com/dmacvicar/terraform-provider-libvirt/releases/download/v0.6.3/terraform-provider-libvirt-0.6.3+git.1604843676.67f4f2aa.Ubuntu_20.04.amd64.tar.gz
+
+```
---
+++ main.tf
@@ -0,0 +1,113 @@
+terraform{
+  required_providers {
+    libvirt = {
+      source = "dmacvicar/libvirt"
+        version = "0.6.3"
+    }
+  }
+}
+
+variable "VM_COUNT" {
+  default = 1
+  type = number
+}
+
+variable "VM_USER" {
+  default = "mheni"
+  type = string
+}
+
+variable "VM_HOSTNAME" {
+  default = "vm4"
+  type = string
+}
+
+variable "VM_IMG_URL" {
+  default = "https://download.cirros-cloud.net/0.6.1/cirros-0.6.1-arm-disk.img"
+  type = string
+}
+
+variable "VM_IMG_FORMAT"{
+  default = "qcow2"
+  type = string
+}
+
+variable "VM_CIDR_RANGE"{
+  default = "10.11.200.0/24"
+  type = string
+}
+
+provider "libvirt"{
+  uri = "qemu:///system"
+}
+
+data "template_file" "user_data" {
+  template = file("${path.module}/cloud_init.cfg")
+  vars = {
+    VM_USER = var.VM_USER
+  }
+}
+
+data "template_file" "network_config"{
+  template = file("${path.module}/network_config.cfg")
+}
+
+resource "libvirt_pool" "vm"{
+  name = "${var.VM_HOSTNAME}_pool"
+  type = "dir"
+  path = "/tmp/terraform-provider-libvirt-pool-cirros-${var.VM_HOSTNAME}"
+}
+
+resource "libvirt_volume" "vm"{
+  count = var.VM_COUNT
+  name = "${var.VM_HOSTNAME}-${count.index}_volume.${var.VM_IMG_FORMAT}"
+  pool = libvirt_pool.vm.name
+  source = var.VM_IMG_URL
+  format = var.VM_IMG_FORMAT
+}
+
+resource "libvirt_network" "vm_public_network"{
+  name = "${var.VM_HOSTNAME}_network"
+  mode = "nat"
+  domain = "${var.VM_HOSTNAME}.local"
+  addresses = ["${var.VM_CIDR_RANGE}"]
+  dhcp{
+    enabled = true
+  }
+  dns{
+    enabled = true
+  }
+}
+
+resource "libvirt_cloudinit_disk" "cloudinit"{
+  name 			= "${var.VM_HOSTNAME}_cloudinit.iso"
+  user_data 		= data.template_file.user_data.rendered
+  network_config 	= data.template_file.network_config.rendered
+  pool			= libvirt_pool.vm.name
+}
+
+resource "libvirt_domain" "vm"{
+  count = var.VM_COUNT
+  name = "${var.VM_HOSTNAME}-${count.index}"
+  memory = "256"
+  vcpu = 1
+
+  cloudinit = "${libvirt_cloudinit_disk.cloudinit.id}"
+  network_interface{
+    network_id = "${libvirt_network.vm_public_network.id}"
+    network_name = "${libvirt_network.vm_public_network.name}"
+  }
+
+  console {
+    type = "pty"
+    target_port = "0"
+    target_type ="serial"
+  }
+
+  disk {
+    volume_id = "${libvirt_volume.vm[count.index].id}"
+  }
+}
+
+
+
