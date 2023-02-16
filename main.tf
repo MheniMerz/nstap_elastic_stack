d5e25b02f71b1f5645aa9ebd107083d135438cd6
mhenimerz <hani.merzouki@gmail.com>
2023-02-16 16:11:31 -0500

    centralize common variables in the root module and pass the values to child modules

--- main.tf
+++ main.tf
@@ -21,4 +21,12 @@ module "es_hot_01"{
 
 module "es_hot_02"{
   source = "./modules/es_hot_02"
+  HTTP_PROXY = var.HTTP_PROXY
+  KVM_HOST_USER = var.KVM_HOST_USER
+  KVM_HOST_IP = "10.11.200.39"
+  KVM_HOST_SSH_KEY = var.KVM_HOST_SSH_KEY
+  VM_USER = var.VM_USER
+  VM_IMG_FORMAT = var.VM_IMG_FORMAT
+  VM_IMG_URL = var.VM_IMG_URL
+  VM_CIDR_RANGE = var.VM_CIDR_RANGE
 }
