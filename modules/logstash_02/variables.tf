4d0b5f190cac09cf463e777d71e636b170d89df5
mhenimerz <hani.merzouki@gmail.com>
2023-02-17 13:36:05 -0500

    rename module logstash_02 and add cloudinit script

--- modules/logstash_02/variables.tf
+++ modules/logstash_02/variables.tf
@@ -5,42 +5,47 @@ variable "KVM_HOST_USER" {
 variable "KVM_HOST_IP" {
   type = string
 }
+
+variable "KVM_HOST_SSH_KEY"{
+  type = string
+}
+
+variable "HTTP_PROXY"{
+  type = string
+}
+
 variable "VM_HOSTNAME" {
   default = "logstash-02"
   type = string
 }
 
 variable "VM_MEMORY"{
-  #60GB
-  default = 61440
+  #32GB
+  default = 32768
 }
 
 variable "VM_CPU"{
-  # 22 Cores
-   default = 22
+  # 16 Cores
+  default = 16
 }
 
 variable "VM_DISK_SIZE"{
-  #1Tb
-  default = 1024*1024*1024*1024
+  #140G
+  default = 1024*1024*1024*320
 }
 
 variable "VM_USER" {
-  default = "mheni"
   type = string
 }
 
 variable "VM_IMG_FORMAT"{
-  default = "qcow2"
   type = string
 }
 
 variable "VM_IMG_URL" {
-  default = "http://cloud-images.ubuntu.com/releases/bionic/release-20191008/ubuntu-18.04-server-cloudimg-amd64.img"
   type = string
 }
 
 variable "VM_CIDR_RANGE"{
-  default = "192.168.0.0/24"
   type = string
 }
