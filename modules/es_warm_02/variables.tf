4deb5b3e8404e398351b9addc0539ffa6af8ed80
mhenimerz <hani.merzouki@gmail.com>
2023-02-17 13:37:12 -0500

    add module es_warm_02

--- modules/es_warm_02/variables.tf
+++ modules/es_warm_02/variables.tf
@@ -5,6 +5,15 @@ variable "KVM_HOST_USER" {
 variable "KVM_HOST_IP" {
   type = string
 }
+
+variable "KVM_HOST_SSH_KEY"{
+  type = string
+}
+
+variable "HTTP_PROXY"{
+  type = string
+}
+
 variable "VM_HOSTNAME" {
   default = "elasticsearch-warm-02"
   type = string
@@ -17,30 +26,26 @@ variable "VM_MEMORY"{
 
 variable "VM_CPU"{
   # 22 Cores
-   default = 22
+  default = 22
 }
 
 variable "VM_DISK_SIZE"{
-  #1Tb
-  default = 1024*1024*1024*1024
+  #140G
+  default = 1024*1024*1024*400
 }
 
 variable "VM_USER" {
-  default = "mheni"
   type = string
 }
 
 variable "VM_IMG_FORMAT"{
-  default = "qcow2"
   type = string
 }
 
 variable "VM_IMG_URL" {
-  default = "http://cloud-images.ubuntu.com/releases/bionic/release-20191008/ubuntu-18.04-server-cloudimg-amd64.img"
   type = string
 }
 
 variable "VM_CIDR_RANGE"{
-  default = "192.168.0.0/24"
   type = string
 }
