35184cb3e5607d450054fa14f0451c43f77c494c
mhenimerz <hani.merzouki@gmail.com>
2023-09-13 14:27:36 +0000

    removed unecessary files

--- main.tf
+++ main.tf
@@ -7,8 +7,8 @@ terraform{
   }
 }
 
-module "kibana"{
-  source = "./modules/kibana"
+module "test_vm"{
+  source = "./modules/test_vm"
   HTTP_PROXY = var.HTTP_PROXY
   KVM_HOST_USER = var.KVM_HOST_USER
   KVM_HOST_IP = "10.11.200.39"
@@ -18,100 +18,3 @@ module "kibana"{
   VM_IMG_URL = var.VM_IMG_URL
   VM_CIDR_RANGE = var.VM_CIDR_RANGE
 }
-
-module "es_master"{
-  source = "./modules/es_master"
-  HTTP_PROXY = var.HTTP_PROXY
-  KVM_HOST_USER = var.KVM_HOST_USER
-  KVM_HOST_IP = "10.11.200.37"
-  KVM_HOST_SSH_KEY = var.KVM_HOST_SSH_KEY
-  VM_USER = var.VM_USER
-  VM_IMG_FORMAT = var.VM_IMG_FORMAT
-  VM_IMG_URL = var.VM_IMG_URL
-  VM_CIDR_RANGE = var.VM_CIDR_RANGE
-}
-
-module "es_hot_01"{
-  source = "./modules/es_hot_01"
-  HTTP_PROXY = var.HTTP_PROXY
-  KVM_HOST_USER = var.KVM_HOST_USER
-  KVM_HOST_IP = "10.11.200.37"
-  KVM_HOST_SSH_KEY = var.KVM_HOST_SSH_KEY
-  VM_USER = var.VM_USER
-  VM_IMG_FORMAT = var.VM_IMG_FORMAT
-  VM_IMG_URL = var.VM_IMG_URL
-  VM_CIDR_RANGE = var.VM_CIDR_RANGE
-}
-
-module "es_hot_02"{
-  source = "./modules/es_hot_02"
-  HTTP_PROXY = var.HTTP_PROXY
-  KVM_HOST_USER = var.KVM_HOST_USER
-  KVM_HOST_IP = "10.11.200.37"
-  KVM_HOST_SSH_KEY = var.KVM_HOST_SSH_KEY
-  VM_USER = var.VM_USER
-  VM_IMG_FORMAT = var.VM_IMG_FORMAT
-  VM_IMG_URL = var.VM_IMG_URL
-  VM_CIDR_RANGE = var.VM_CIDR_RANGE
-}
-
-module "es_warm_01"{
-  source = "./modules/es_warm_01"
-  HTTP_PROXY = var.HTTP_PROXY
-  KVM_HOST_USER = var.KVM_HOST_USER
-  KVM_HOST_IP = "10.11.200.38"
-  KVM_HOST_SSH_KEY = var.KVM_HOST_SSH_KEY
-  VM_USER = var.VM_USER
-  VM_IMG_FORMAT = var.VM_IMG_FORMAT
-  VM_IMG_URL = var.VM_IMG_URL
-  VM_CIDR_RANGE = var.VM_CIDR_RANGE
-}
-
-module "es_warm_02"{
-  source = "./modules/es_warm_02"
-  HTTP_PROXY = var.HTTP_PROXY
-  KVM_HOST_USER = var.KVM_HOST_USER
-  KVM_HOST_IP = "10.11.200.38"
-  KVM_HOST_SSH_KEY = var.KVM_HOST_SSH_KEY
-  VM_USER = var.VM_USER
-  VM_IMG_FORMAT = var.VM_IMG_FORMAT
-  VM_IMG_URL = var.VM_IMG_URL
-  VM_CIDR_RANGE = var.VM_CIDR_RANGE
-}
-
-module "es_cold_01"{
-  source = "./modules/es_cold_01"
-  HTTP_PROXY = var.HTTP_PROXY
-  KVM_HOST_USER = var.KVM_HOST_USER
-  KVM_HOST_IP = "10.11.200.39"
-  KVM_HOST_SSH_KEY = var.KVM_HOST_SSH_KEY
-  VM_USER = var.VM_USER
-  VM_IMG_FORMAT = var.VM_IMG_FORMAT
-  VM_IMG_URL = var.VM_IMG_URL
-  VM_CIDR_RANGE = var.VM_CIDR_RANGE
-}
-
-module "logstash_01"{
-  source = "./modules/logstash_01"
-  HTTP_PROXY = var.HTTP_PROXY
-  KVM_HOST_USER = var.KVM_HOST_USER
-  KVM_HOST_IP = "10.11.200.40"
-  KVM_HOST_SSH_KEY = var.KVM_HOST_SSH_KEY
-  VM_USER = var.VM_USER
-  VM_IMG_FORMAT = var.VM_IMG_FORMAT
-  VM_IMG_URL = var.VM_IMG_URL
-  VM_CIDR_RANGE = var.VM_CIDR_RANGE
-}
-
-module "logstash_02"{
-  source = "./modules/logstash_02"
-  HTTP_PROXY = var.HTTP_PROXY
-  KVM_HOST_USER = var.KVM_HOST_USER
-  KVM_HOST_IP = "10.11.200.40"
-  KVM_HOST_SSH_KEY = var.KVM_HOST_SSH_KEY
-  VM_USER = var.VM_USER
-  VM_IMG_FORMAT = var.VM_IMG_FORMAT
-  VM_IMG_URL = var.VM_IMG_URL
-  VM_CIDR_RANGE = var.VM_CIDR_RANGE
-}
-
--- modules/logstash_01/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "OUTPUT_IP"{
-  value = libvirt_domain.vm.network_interface[0].addresses[0]
-}
